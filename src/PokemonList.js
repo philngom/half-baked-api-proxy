57caaf7f9fe82fe25efa28a77e1af11153752c39
Philippe Ngom <philngom@gmail.com>
2022-02-14 15:21:19 -0800

    wrote out handlePokemonSubmit functionality

---
+++ src/PokemonList.js
@@ -0,0 +1,13 @@
+export default function PokemonList({ pokemons }) {
+  return (
+    <div>
+      {/* {
+        pokemons.map((pokemon, i) =>
+        <div key={pokemon + i}>
+              pokemon.pokemon
+            <div/>
+        )
+      } */}
+    </div>
+  );
+}
